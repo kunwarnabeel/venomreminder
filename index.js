917495729ce2208c22a314c6a5e61e5c0634eb00
kunwarnabeel <kunwar.nabeel@hotmail.com>
2021-08-19 19:34:23 +0500

    venomreminders

---
+++ index.js
@@ -0,0 +1,114 @@
+require('dotenv').config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const venom = require('venom-bot');
+
+//APP config
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+
+//DB config
+mongoose.connect('mongodb://localhost:27017/reminderAppDB', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, () => console.log("DB connected"))
+const reminderSchema = new mongoose.Schema({
+    reminderMsg: String,
+    remindAt: String,
+    isReminded: Boolean
+})
+const Reminder = new mongoose.model("reminder", reminderSchema)
+
+//Whatsapp reminding functionality
+
+venom
+.create()
+.then((client) => start(client))
+.catch((erro) => {
+    console.log(erro);
+});
+
+function start(client) {
+setInterval(() => {
+    console.log("here");
+    Reminder.find({}, (err, reminderList) => {
+        if(err) {
+            console.log(err)
+        }
+        if(reminderList){
+            reminderList.forEach(reminder => {
+                if(!reminder.isReminded){
+                    const now = new Date()
+                    if((new Date(reminder.remindAt) - now) < 0) {
+                        Reminder.findByIdAndUpdate(reminder._id, {isReminded: true}, (err, remindObj)=>{
+                            if(err){
+                                console.log(err)
+                            }
+                            client.sendText('923352999478@c.us', reminder.reminderMsg).then((result) => {
+                                console.log('Result: ', result); //return object success
+                              })
+                              .catch((erro) => {
+                                console.error('Error when sending: ', erro); //return object error
+                              });
+                        })
+                    }
+                }
+            })
+        }
+    })
+},1000);
+
+}
+
+
+//API routes
+app.get("/getAllReminder", (req, res) => {
+    Reminder.find({}, (err, reminderList) => {
+        if(err){
+            console.log(err)
+        }
+        if(reminderList){
+            res.send(reminderList)
+        }
+    })
+})
+app.post("/addReminder", (req, res) => {
+    const { reminderMsg, remindAt } = req.body
+    const reminder = new Reminder({
+        reminderMsg,
+        remindAt,
+        isReminded: false
+    })
+    reminder.save(err => {
+        if(err){
+            console.log(err)
+        }
+        Reminder.find({}, (err, reminderList) => {
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    })
+
+})
+app.post("/deleteReminder", (req, res) => {
+    Reminder.deleteOne({_id: req.body.id}, () => {
+        Reminder.find({}, (err, reminderList) => {
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    })
+})
+
+app.listen(9000, () => console.log("Be started"))
